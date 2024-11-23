78ed32c8bf25257961cf1a47873f241e6a3ab615
Vishal-h-s <yeahiamvishal@gmail.com>
2024-11-23 13:27:32 +0530

    first

---
+++ src/main/java/com/mavenJavaProject/myProject/App.java
@@ -0,0 +1,13 @@
+package com.mavenJavaProject.myProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
